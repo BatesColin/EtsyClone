4534117046a862849ceb6d2a4b31be22fbd17619
cb <dragonflylord@gmail.com>
2018-08-24 13:42:52 -0700

    it will now have CRUD functions for products.

--- src/app/product/info/product.service.ts
+++ src/app/product/info/product.service.ts
@@ -1,10 +1,42 @@
 import { Injectable } from '@angular/core';
-import { employee } from './employee.model';
+import { Product } from './product.model';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  constructor() { }
+  product: AngularFireList<any>;
+  selectedProduct: Product = new Product ();
+  constructor(private firebase :AngularFireDatabase) { }
 }
+
+  getData(){
+    this.productList = this.firebase.list('products')
+    return this.productList;
+  }
+
+  insertProduct(product : Product)
+  {
+    this.productList.push({
+
+      name : product.name,
+      price : product.price,
+      seller : product.seller,
+      availible : product.availible
+    });
+
+    updateProduct(product : Product){
+      this.productList.update(product.$key
+      {
+        name : product.name,
+        price : product.price,
+        seller : product.seller,
+        availible : product.availible
+      });
+    }
+
+    deleteProduct($key : string){
+      this.productList.remove($key)
+    }
+
+  }
