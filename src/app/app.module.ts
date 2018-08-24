8e58595d0597eedc18122558d8a14a7611ccdaac
cb <dragonflylord@gmail.com>
2018-08-24 11:00:44 -0700

    It will now have more Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { angularFireDatabaseModule } from '@Angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '..environments/environment';
+import { ProductComponent } from './product/product.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    angularFireDatabaseModule.
   ],
   providers: [],
   bootstrap: [AppComponent]
