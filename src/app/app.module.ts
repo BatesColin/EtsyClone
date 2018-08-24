33c4c442b3ec97d5413abf2c45305a2e7f0d4f44
cb <dragonflylord@gmail.com>
2018-08-24 16:44:59 -0700

    it will now have files linking correctly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angularfire2';
-import { angularFireDatabaseModule } from '@angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { angularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
-import { environment } from '..environments/environment';
+import { environment } from '../environments/environment';
 import { ProductComponent } from './product/product.component';
 import { EmployeeComponent } from './employee/employee.component';
-import { ProductsComponent } from './product/product/product.component';
+import { ProductService } from './product/info/product.service';
 @NgModule({
   declarations: [
     AppComponent,
