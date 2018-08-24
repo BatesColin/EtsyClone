a8534e68a4a03e8547f6c5963182e6804d89e995
cb <dragonflylord@gmail.com>
2018-08-24 15:46:52 -0700

    it will now have more views.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angularfire2';
-import { angularFireDatabaseModule } from '@Angularfire2/database';
+import { angularFireDatabaseModule } from '@angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '..environments/environment';
 import { ProductComponent } from './product/product.component';
 import { EmployeeComponent } from './employee/employee.component';
-
+import { ProductsComponent } from './product/product/product.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    ProductsComponent
+
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    angularFireDatabaseModule.
+    angularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
