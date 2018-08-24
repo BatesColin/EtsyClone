74c78065f40261feae625a5b42cd8819578910d4
cb <dragonflylord@gmail.com>
2018-08-24 12:05:41 -0700

    it will now have product info framework.

--- src/app/product/info/product.service.ts
+++ src/app/product/info/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { employee } from './employee.model';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 @Injectable({
   providedIn: 'root'
 })
